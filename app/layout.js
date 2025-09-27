c7796ff4860c5debece9b361175de2b49a7d0f0e
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2025-09-27 21:22:40 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toggle } from "@/components/toggle"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,11 +21,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div><Toggle /></div>
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
